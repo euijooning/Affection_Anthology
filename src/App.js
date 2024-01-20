0cf120db2634993c44e21f298afb963df72e78c8
EUIJOON <euijoonism@gmail.com>
2024-01-20 14:51:13 +0900

    [작업] image assets 세팅

--- src/App.js
+++ src/App.js
@@ -7,11 +7,22 @@ import Edit from "./pages/Edit";
 import Log from "./pages/Log";
 
 function App() {
+
+  // const env = process.env;
+  // env.PUBLIC_URL = env.PUBLIC_URL || "";
+
   return (
     <BrowserRouter>
       <div className="App">
         <h1>App.js file</h1>
         <p>메인 페이지입니다.</p>
+
+        {/* <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} alt="img" />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} alt="img" />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} alt="img" />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} alt="img" />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} alt="img" /> */}
+
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/create" element={<Create />}/>
