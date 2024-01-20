12478eb748d5e4daa51d1c5aa81a0dcf072ff995
EUIJOON <euijoonism@gmail.com>
2024-01-20 16:03:50 +0900

    [작업] 헤더 컴포넌트 기본 설정값 세팅

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ import Create from "./pages/Create";
 import Edit from "./pages/Edit";
 import Log from "./pages/Log";
 import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
 
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader headText={"App!!!"} 
+          leftChild= { <MyButton text={"왼쪽버튼"} onClick={() => alert("왼쪽 클릭")} /> }
+          rightChild={ <MyButton text={"오른쪽버튼"} onClick={() => alert("오른쪽 클릭")} /> }
+        />
+
         <h1>App.js file</h1>
         <p>메인 페이지입니다.</p>
         
