aa9c15f2c00e298b135eb5cd97e8e7638dba9f9f
EUIJOON <euijoonism@gmail.com>
2024-01-21 00:47:18 +0900

    [작업] 일기 리스트 최신순/오래된 순에 따라 정렬시키는 기능 구현

--- src/components/LogList.js
+++ src/components/LogList.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+
+const sortOptionList = [
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
 
 const LogList = ({ logList }) => {
+  // 정렬 기준을 저장할 state
+  const [sortType, setSortType] = useState("latest");
+
+  // 최신순 오래된순에 따라서 일기 정렬
+  const getProcessedLogList = () => {
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+
+    const copyList = JSON.parse(JSON.stringify(logList));
+    const sortedList = copyList.sort(compare);
+    return sortedList;
+  };
+
   return (
     <div>
-      {logList.map((it) => (
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={sortOptionList}
+      />
+      {getProcessedLogList().map((it) => (
         <div key={it.id}>{it.content}</div>
       ))}
     </div>
