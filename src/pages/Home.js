041269ed3a8419e46c41dded3a00b54a64d81810
EUIJOON <euijoonism@gmail.com>
2024-01-20 21:06:18 +0900

    [작업] 상단 양 옆 버튼 기능까지 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
-import MyHeader from "./../components/MyHeader"
+import MyHeader from "./../components/MyHeader";
+import MyButton from "./../components/MyButton";
 
 const Home = () => {
   const [nowDate, setNowDate] = useState(new Date());
-  console.log(nowDate);
+  // console.log(nowDate);
 
-  const headText = `${nowDate.getFullYear()}년 ${nowDate.getMonth() + 1}월`
+  const headText = `${nowDate.getFullYear()}년 ${nowDate.getMonth() + 1}월`;
 
-  return <div>
-    <MyHeader headText={headText} />
-  </div>;
+  const increaseMonth = () => {
+    setNowDate(
+      new Date(nowDate.getFullYear(), nowDate.getMonth() + 1, nowDate.getDate())
+    );
+  };
+
+  const decreaseMonth = () => {
+    setNowDate(
+      new Date(nowDate.getFullYear(), nowDate.getMonth() - 1, nowDate.getDate())
+    );
+  };
+
+  return (
+    <div>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      />
+    </div>
+  );
 };
 
 export default Home;
