447f49412bea5b09ff5de884a4010339d2cc3fd5
EUIJOON <euijoonism@gmail.com>
2024-01-20 13:49:40 +0900

    [작업] Query String 사용 환경 세팅

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom';
 
 const Edit = () => {
+  
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id :" + id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode : " + mode);
+
   return (
     <div>
-      <h1>New</h1>
-      <p>This is New page</p>
+      <h1>Edit</h1>
+      <p>This is Edit page</p>
+      <button onClick={() => setSearchParams({who : "newjeans"})}> 쿼리스트링 변경 </button>
     </div>
-  )
-}
+  );
+};
 
 export default Edit;
