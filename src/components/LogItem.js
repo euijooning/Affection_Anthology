1db60e3e5cfaa2bf453f8ec01fd1befd4423fdfa
EUIJOON <euijoonism@gmail.com>
2024-01-21 11:26:40 +0900

    [작업] 일기 상세 / 수정하기 페이지 이동 기능을 추가

--- src/components/LogItem.js
+++ src/components/LogItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MyButton from "./MyButton";
+import { useNavigate } from "react-router-dom";
 
 const LogItem = ({ id, emotion, content, date }) => {
   // 혹시 모르니 예외처리
@@ -8,9 +9,18 @@ const LogItem = ({ id, emotion, content, date }) => {
 
   const strDate = new Date(parseInt(date)).toLocaleDateString();
 
+  const navigate = useNavigate();
+  const moveLogDetail = () => {
+    navigate(`/log/${id}`);
+  };
+  const moveEditPage = () => {
+    navigate(`/edit/${id}`)
+  }
+
   return (
     <div className="LogItem">
       <div
+        onClick={moveLogDetail}
         className={[
           "emotion_img_wrapper",
           `emotion_img_wrapper_${emotion}`,
@@ -19,12 +29,12 @@ const LogItem = ({ id, emotion, content, date }) => {
         {/* 여기 이미지 */}
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
       </div>
-      <div className="info_wrapper">
+      <div onClick={moveLogDetail} className="info_wrapper">
         <div className="log_date">{strDate}</div>
-        <div className="log_content_preview">{content.slice(0,25)}</div>
+        <div className="log_content_preview">{content.slice(0, 25)}</div>
       </div>
       <div className="button_wrapper">
-        <MyButton text={"수정하기"} />
+        <MyButton onClick={moveEditPage} text={"수정하기"} />
       </div>
     </div>
   );
