050d12476edc526e8f005d67dacad150a85e83b6
EUIJOON <euijoonism@gmail.com>
2024-01-21 12:47:16 +0900

    [작업] 일기 작성 날짜 선택 섹션

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import MyHeader from "../components/MyHeader";
 import MyButton from "../components/MyButton";
 import { useNavigate } from "react-router-dom";
 
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
+
+
 const Create = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate(getStringDate(new Date()));
+
+  // input_box의 날짜를 핸들링할 state
+  const [date, setDate] = useState();
 
   return (
     <div>
-     <MyHeader 
-      headText={"일기 작성하기"}
-      leftChild={
-        <MyButton text={"< 뒤로"} onClick={() => navigate(-1)} />
-      }
-     /> 
+      <MyHeader
+        headText={"일기 작성하기"}
+        leftChild={<MyButton text={"< 뒤로"} onClick={() => navigate(-1)} />}
+      />
+      <div>
+        <h4>오늘 날짜는?</h4>
+        <div className="input-box">
+          <input
+            className="input-date"
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+      </div>
     </div>
   );
 };
