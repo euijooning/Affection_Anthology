6c3c05024bf3255fd657ebc9462434d1ed499624
EUIJOON <euijoonism@gmail.com>
2024-01-21 12:31:56 +0900

    [작업] 일기 작성 페이지 가장 상단 부분 구현

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <h1>Create</h1>
-      <p>This is New page</p>
+     <MyHeader 
+      headText={"일기 작성하기"}
+      leftChild={
+        <MyButton text={"< 뒤로"} onClick={() => navigate(-1)} />
+      }
+     /> 
     </div>
-  )
-}
+  );
+};
 
 export default Create;
